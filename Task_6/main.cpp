a8043afbe0b611489b5e8d101bc1bafcc033e0d6
ClosedEnotik <solbanka@gmail.com>
2024-10-12 14:17:12 +0300

    Add files via upload

---
+++ Task_6/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+void printTriangle1() {
+    std::cout << "    *    \n";
+    std::cout << "   **    \n";
+    std::cout << "  ***    \n";
+    std::cout << " ****    \n";
+    std::cout << "*****    \n";
+}
+
+
+void printTriangle2() {
+    std::cout << "*****\n";
+    std::cout << "**** \n";
+    std::cout << "***  \n";
+    std::cout << "**   \n";
+    std::cout << "*    \n";
+}
+
+
+void printTriangle3() {
+    std::cout << "*****\n";
+    std::cout << " ****\n";
+    std::cout << "  ***\n";
+    std::cout << "   **\n";
+    std::cout << "    *\n";
+}
+
+
+void printTriangle4() {
+    std::cout << "    *\n";
+    std::cout << "   **\n";
+    std::cout << "  ***\n";
+    std::cout << " ****\n";
+    std::cout << "*****\n";
+}
+
+int main() {
+    while (true) {
+        clearScreen();
+        printTriangle1();
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+        clearScreen();
+        printTriangle2();
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+        clearScreen();
+        printTriangle3();
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+        clearScreen();
+        printTriangle4();
+        std::this_thread::sleep_for(std::chrono::milliseconds(300));
+    }
+
+    return 0;
+}
