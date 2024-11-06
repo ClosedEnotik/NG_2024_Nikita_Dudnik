7b6e63f9e262a4b8f9d71b2dcd69ed8ad08eb73c
ClosedEnotik <solbanka@gmail.com>
2024-11-06 18:00:12 +0200

    Task_4

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+
+    cout << "How much money do you make?: ";
+    cin >> number;
+
+    if (number > 1000 and number < 999999) {
+        cout << "You`re good" << endl;
+    }
+
+    if (number < 1000 ) {
+        cout << "You must work more" << endl;
+    }
+
+    if (number > 1000000) {
+        cout << "So you`re a millionaire " << endl;
+    }
+
+    return 0;
+}
